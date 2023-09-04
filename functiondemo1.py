a3b4f7765729ae3267f2da7507befa2caf49108e
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-09-04 11:17:55 +0530

    New file functiondemo1.py is created

--- functiondemo1.py
+++ functiondemo1.py
@@ -1,3 +1,64 @@
+
+def display():
+    def message():
+        return "How are you?"
+    return message
+
+fun = display()
+print(fun())
+
+
+
+
+
+
+import sys
+sys.exit()
+
+def display(str):
+    def message():
+        return "How are you?"
+    result = message() + str
+    return result
+
+print(display(" Sagar"))
+
+
+
+
+
+
+
+
+import sys
+sys.exit()
+def display(str):
+    return "Hi" + str
+
+x= display(" Sagar")
+print(x)
+
+
+import sys
+sys.exit()
+def calcuate_sum_list(lst):
+    sum=0
+    for item in lst:
+        sum=sum+int(item)
+    return sum
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+result = calcuate_sum_list(lst)
+print("The sum of all items in list=",result)
+
+
+
+
+
+
+
+import sys
+sys.exit()
 def calculate(a,b):
     sum = a+b
     sub = a-b
